646ecabff34ab47c570403c503ea99cff6d73536
Draylar <samuelhimes@gmail.com>
2020-03-05 14:11:05 -0600

    refactor ClaimUtils to be more efficient, add JavaDocs

--- src/main/java/draylar/goml/api/ClaimUtils.java
+++ src/main/java/draylar/goml/api/ClaimUtils.java
@@ -22,34 +22,57 @@ public class ClaimUtils {
      * @return  claims at the given position in the given world
      */
     public static Selection<Entry<Box, ClaimInfo>> getClaimsAt(WorldView world, BlockPos pos) {
-        return GetOffMyLawn.CLAIM.get(world).getClaims().entries(box -> box.contains(Box.create(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1)));
+        Box checkBox = Box.create(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1);
+        return GetOffMyLawn.CLAIM.get(world).getClaims().entries(box -> box.contains(checkBox));
     }
 
+    /**
+     * Returns all claims that intersect with a box created by the 2 given positions.
+     *
+     * @param world  world to check for claim in
+     * @param lower  lower corner of claim
+     * @param upper  upper corner of claim
+     * @return  claims that intersect with a box created by the 2 positions in the given world
+     */
     public static Selection<Entry<Box, ClaimInfo>> getClaimsInBox(WorldView world, BlockPos lower, BlockPos upper) {
-        return GetOffMyLawn.CLAIM.get(world).getClaims().entries(box -> box.intersectsClosed(Box.create(lower.getX(), lower.getY(), lower.getZ(), upper.getX(), upper.getY(), upper.getZ())));
+        Box checkBox = Box.create(lower.getX(), lower.getY(), lower.getZ(), upper.getX(), upper.getY(), upper.getZ());
+        return GetOffMyLawn.CLAIM.get(world).getClaims().entries(box -> box.intersectsClosed(checkBox));
     }
 
+    /**
+     * Returns all claims that intersect with a box created by the 2 given positions.
+     * If the found box is equal to the ignore box, it is not included.
+     *
+     * @param world  world to check for claim in
+     * @param lower  lower corner of claim
+     * @param upper  upper corner of claim
+     * @param ignore  box to ignore
+     * @return  claims that intersect with a box created by the 2 positions in the given world
+     */
     public static Selection<Entry<Box, ClaimInfo>> getClaimsInBox(WorldView world, BlockPos lower, BlockPos upper, Box ignore) {
-        return GetOffMyLawn.CLAIM.get(world).getClaims().entries(box -> box.intersectsClosed(Box.create(lower.getX(), lower.getY(), lower.getZ(), upper.getX(), upper.getY(), upper.getZ())) && !box.equals(ignore));
+        Box checkBox = Box.create(lower.getX(), lower.getY(), lower.getZ(), upper.getX(), upper.getY(), upper.getZ());
+        return GetOffMyLawn.CLAIM.get(world).getClaims().entries(box -> box.intersectsClosed(checkBox) && !box.equals(ignore));
     }
 
-    public static boolean doesBoxHaveClaimsNotOwnedBy(WorldView world, BlockPos lower, BlockPos upper, PlayerEntity playerEntity) {
-        Selection<Entry<Box, ClaimInfo>> claimsInBox = getClaimsInBox(world, lower, upper);
-        AtomicBoolean valid = new AtomicBoolean(true);
-
-        claimsInBox.forEach(boxClaimInfoEntry -> {
-            if(!boxClaimInfoEntry.getValue().getOwner().equals(playerEntity.getUuid())) {
-                valid.set(false);
-            }
-        });
-
-        return valid.get();
-    }
-
+    /**
+     * Returns whether or not the information about a claim matches with a {@link PlayerEntity} and {@link BlockPos}.
+     *
+     * @param claim  claim to check
+     * @param checkPlayer  player to check against
+     * @param checkPos  position to check against
+     * @return  whether or not the claim information matches up with the player and position
+     */
     public static boolean claimMatchesWith(Entry<Box, ClaimInfo> claim, PlayerEntity checkPlayer, BlockPos checkPos) {
         return playerHasPermission(claim, checkPlayer) && claim.getValue().getOrigin().equals(checkPos);
     }
 
+    /**
+     * Returns whether the given {@link PlayerEntity} has permission to do anything in the given claim.
+     *
+     * @param claim  claim to check for permisisons
+     * @param checkPlayer  player to check
+     * @return  whether the checkPlayer can build/interact in the claim
+     */
     public static boolean playerHasPermission(Entry<Box, ClaimInfo> claim, PlayerEntity checkPlayer) {
         return claim.getValue().getOwner().equals(checkPlayer.getUuid());
     }
