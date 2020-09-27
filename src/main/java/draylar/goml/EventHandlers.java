b03d20c5a5f41c148a8f3c75d7fea3210ea35bfe
Draylar <samuelhimes@gmail.com>
2020-09-27 12:13:08 -0500

    change || to && for permission check to fix claims, refactor event handlers, 1.2.1 beta

--- src/main/java/draylar/goml/EventHandlers.java
+++ src/main/java/draylar/goml/EventHandlers.java
@@ -9,10 +9,15 @@ import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 
 public class EventHandlers {
+
+    public static final TranslatableText BLOCK_PROTECTED = new TranslatableText("goml.block_protected");
+    public static final TranslatableText ENTITY_PROTECTED = new TranslatableText("goml.entity_protected");
+    public static final TranslatableText AREA_PROTECTED = new TranslatableText("goml.area_protected");
 
     private EventHandlers() {
         // NO-OP
@@ -28,84 +33,47 @@ public class EventHandlers {
     private static void registerInteractEntityCallback() {
         UseEntityCallback.EVENT.register((playerEntity, world, hand, entity, entityHitResult) -> {
             Selection<Entry<ClaimBox, Claim>> claimsFound = ClaimUtils.getClaimsAt(world, entity.getBlockPos());
-
-            if(!claimsFound.isEmpty()) {
-                boolean noPermission = claimsFound.anyMatch((Entry<ClaimBox, Claim> boxInfo) -> !boxInfo.getValue().hasPermission(playerEntity));
-
-                if(noPermission || !playerEntity.hasPermissionLevel(3)) {
-                    playerEntity.sendMessage(new TranslatableText("goml.entity_protected"), true);
-                    return ActionResult.FAIL;
-                }
-            }
-
-            return ActionResult.PASS;
+            return testPermission(claimsFound, playerEntity, ENTITY_PROTECTED);
         });
     }
 
     private static void registerAttackEntityCallback() {
-        AttackEntityCallback.EVENT.register(((playerEntity, world, hand, entity, entityHitResult) -> {
+        AttackEntityCallback.EVENT.register((playerEntity, world, hand, entity, entityHitResult) -> {
             Selection<Entry<ClaimBox, Claim>> claimsFound = ClaimUtils.getClaimsAt(world, entity.getBlockPos());
-
-            if(!claimsFound.isEmpty()) {
-                boolean noPermission = claimsFound.anyMatch((Entry<ClaimBox, Claim> boxInfo) -> !boxInfo.getValue().hasPermission(playerEntity));
-
-                if(noPermission || !playerEntity.hasPermissionLevel(3)) {
-                    playerEntity.sendMessage(new TranslatableText("goml.entity_protected"), true);
-                    return ActionResult.FAIL;
-                }
-            }
-
-            return ActionResult.PASS;
-        }));
+            return testPermission(claimsFound, playerEntity, ENTITY_PROTECTED);
+        });
     }
 
     private static void registerInteractBlockCallback() {
-        UseBlockCallback.EVENT.register(((playerEntity, world, hand, blockHitResult) -> {
+        UseBlockCallback.EVENT.register((playerEntity, world, hand, blockHitResult) -> {
             Selection<Entry<ClaimBox, Claim>> claimsFound = ClaimUtils.getClaimsAt(world, blockHitResult.getBlockPos());
-
-            if(!claimsFound.isEmpty()) {
-                boolean noPermission = claimsFound.anyMatch((Entry<ClaimBox, Claim> boxInfo) -> !boxInfo.getValue().hasPermission(playerEntity));
-
-                if(noPermission || !playerEntity.hasPermissionLevel(3)) {
-                    playerEntity.sendMessage(new TranslatableText("goml.block_protected"), true);
-                    return ActionResult.FAIL;
-                }
-            }
-
-            return ActionResult.PASS;
-        }));
+            return testPermission(claimsFound, playerEntity, AREA_PROTECTED);
+        });
 
         // handle placing blocks at side of block not in claim
-        UseBlockCallback.EVENT.register(((playerEntity, world, hand, blockHitResult) -> {
+        UseBlockCallback.EVENT.register((playerEntity, world, hand, blockHitResult) -> {
             Selection<Entry<ClaimBox, Claim>> claimsFound = ClaimUtils.getClaimsAt(world, blockHitResult.getBlockPos().offset(blockHitResult.getSide()));
-
-            if(!claimsFound.isEmpty()) {
-                boolean noPermission = claimsFound.anyMatch((Entry<ClaimBox, Claim> boxInfo) -> !boxInfo.getValue().hasPermission(playerEntity));
-
-                if(noPermission || !playerEntity.hasPermissionLevel(3)) {
-                    playerEntity.sendMessage(new TranslatableText("goml.block_protected"), true);
-                    return ActionResult.FAIL;
-                }
-            }
-
-            return ActionResult.PASS;
-        }));
+            return testPermission(claimsFound, playerEntity, AREA_PROTECTED);
+        });
     }
 
     private static void registerBreakBlockCallback() {
         AttackBlockCallback.EVENT.register((playerEntity, world, hand, blockPos, direction) -> {
             Selection<Entry<ClaimBox, Claim>> claimsFound = ClaimUtils.getClaimsAt(world, blockPos);
-
-            if(!claimsFound.isEmpty()) {
-                boolean noPermission = claimsFound.anyMatch((Entry<ClaimBox, Claim> boxInfo) -> !boxInfo.getValue().hasPermission(playerEntity));
-
-                if(noPermission || !playerEntity.hasPermissionLevel(3)) {
-                    playerEntity.sendMessage(new TranslatableText("goml.block_protected"), true);
-                    return ActionResult.FAIL;
-                }
-            }
-
-            return ActionResult.PASS;
+            return testPermission(claimsFound, playerEntity, BLOCK_PROTECTED);
         });
+    }
+
+    private static ActionResult testPermission(Selection<Entry<ClaimBox, Claim>> claims, PlayerEntity player, TranslatableText error) {
+        if(!claims.isEmpty()) {
+            boolean noPermission = claims.anyMatch((Entry<ClaimBox, Claim> boxInfo) -> !boxInfo.getValue().hasPermission(player));
+
+            if(noPermission && !player.hasPermissionLevel(3)) {
+                player.sendMessage(error, true);
+                return ActionResult.FAIL;
+            }
+        }
+
+        return ActionResult.PASS;
     }
 }
